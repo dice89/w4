4e9a5d1c751d0f3b938e989679d30950c23568bb
Alexander Mueller <alex.chr.mueller@gmail.com>
2014-06-15 11:45:25 +0200

    test

--- src/main/java/w4/WikiExtractor.java
+++ src/main/java/w4/WikiExtractor.java
@@ -1,13 +1,14 @@
 package w4;
 
 import info.bliki.api.Page;
-import info.bliki.api.Revision;
 import info.bliki.api.User;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.httpclient.HttpClient;
@@ -19,6 +20,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import net.sourceforge.jwbf.core.contentRep.Article;
 import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
@@ -33,7 +36,7 @@ public class WikiExtractor {
         .setPath("/w/api.php")
         .setParameter("format", "json")
         .setParameter("action", "query")
-        .setParameter("titles", "Crimea")
+        .setParameter("titles", keyword)
          .setParameter("prop", "revisions")
          .setParameter("rvprop", "content|user|timestamp|size|ids|userid|parsedcomment|tags")
         .setParameter("rvlimit", "10")
@@ -57,13 +60,42 @@ public class WikiExtractor {
 			result = result + theLine;
 		}
 		
-		System.out.println(result);
+		//System.out.println(result);
 		
 		return result;
 	}
 	
+	public ArrayList<Revision> parseJSON(String keyword) throws IllegalStateException, URISyntaxException, IOException{
+		ArrayList<Revision> revisionsList = new ArrayList<Revision>();
+		String json = this.getJSONStringFromAPI(keyword);
+		JSONObject obj = new JSONObject(json);
+		
+		JSONObject page = obj.getJSONObject("query").getJSONObject("pages").getJSONObject("163045");
+
+		JSONArray revisions = page.getJSONArray("revisions");
+		
+		for(int i= 0; i <revisions.length(); i++) {
+			JSONObject jobj = (JSONObject) revisions.get(i);
+			System.out.println(jobj.getInt("revid"));
+			
+			Revision rev = new Revision(jobj.getString("user"), jobj.getInt("userid")+"", jobj.getString("timestamp"),jobj.getInt("size"), jobj.getString("*"), jobj.getString("comment"));
+			revisionsList.add(rev);
+			
+			
+		}
+		
+		return revisionsList;
+		//System.out.println(page);
+		
+		
+		
+	}
+	
 	public static void main (String args[]) throws URISyntaxException, ClientProtocolException, IOException{
 		
+		WikiExtractor e = new WikiExtractor();
+		e.parseJSON("Crimea");
+	
 	}
 	
 }
